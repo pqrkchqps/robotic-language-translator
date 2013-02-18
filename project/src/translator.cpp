42a09f3e9b4527495bb9260a9a401458fa9e04e6
Thuy T Nguyen <nguy1638@kh1260-07.cselabs.umn.edu>
2013-02-18 09:36:59 -0600

    Add new file

---
+++ project/src/translator.cpp
@@ -0,0 +1,29 @@
+/* Robotics DSL based on state machines.
+
+   Authors: Eric Van Wyk
+
+   This file contains the main function that controls the execution of
+   the translator.
+
+   It first reads the input program into a character buffer.
+
+   If no erroros occur, it calls the scanner.
+
+ */
+
+#include <iostream>
+
+using namespace std ;
+
+int main(int argc, char **argv) {
+
+    cout << "Getting started with the translator project." << endl ;
+
+    /* Each of you should separately add a line to print your name,
+       following the instructions for the lab. */
+  
+
+    /* P1: change "program" to "application" 
+       P2: remove the word "program". */
+    cout << "Thanks for using the translator program." << endl ;
+}
