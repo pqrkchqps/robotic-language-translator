e4a8ad43c32af0ef5001b77ff118994132c9560a
Thuy Nguyen <nugy1638@umn.edu>
2013-04-17 00:56:50 -0500

    Iter3

---
+++ Project/src/scanner_tests.cpp
@@ -0,0 +1,308 @@
+/* Generated file, do not edit */
+
+#ifndef CXXTEST_RUNNING
+#define CXXTEST_RUNNING
+#endif
+
+#define _CXXTEST_HAVE_STD
+#define _CXXTEST_HAVE_EH
+#define _CXXTEST_ABORT_TEST_ON_FAIL
+#include <cxxtest/TestListener.h>
+#include <cxxtest/TestTracker.h>
+#include <cxxtest/TestRunner.h>
+#include <cxxtest/RealDescriptions.h>
+#include <cxxtest/ErrorPrinter.h>
+
+int main() {
+ return CxxTest::ErrorPrinter().run();
+}
+#include "scanner_tests.h"
+
+static ScannerTestSuite suite_ScannerTestSuite;
+
+static CxxTest::List Tests_ScannerTestSuite = { 0, 0 };
+CxxTest::StaticSuiteDescription suiteDescription_ScannerTestSuite( "scanner_tests.h", 17, "ScannerTestSuite", suite_ScannerTestSuite, Tests_ScannerTestSuite );
+
+static class TestDescription_ScannerTestSuite_test_setup_code : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_setup_code() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 34, "test_setup_code" ) {}
+ void runTest() { suite_ScannerTestSuite.test_setup_code(); }
+} testDescription_ScannerTestSuite_test_setup_code;
+
+static class TestDescription_ScannerTestSuite_test_nameKwd : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_nameKwd() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 42, "test_nameKwd" ) {}
+ void runTest() { suite_ScannerTestSuite.test_nameKwd(); }
+} testDescription_ScannerTestSuite_test_nameKwd;
+
+static class TestDescription_ScannerTestSuite_test_platformKwd : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_platformKwd() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 45, "test_platformKwd" ) {}
+ void runTest() { suite_ScannerTestSuite.test_platformKwd(); }
+} testDescription_ScannerTestSuite_test_platformKwd;
+
+static class TestDescription_ScannerTestSuite_test_initialKwd : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_initialKwd() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 48, "test_initialKwd" ) {}
+ void runTest() { suite_ScannerTestSuite.test_initialKwd(); }
+} testDescription_ScannerTestSuite_test_initialKwd;
+
+static class TestDescription_ScannerTestSuite_test_stateKwd : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_stateKwd() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 51, "test_stateKwd" ) {}
+ void runTest() { suite_ScannerTestSuite.test_stateKwd(); }
+} testDescription_ScannerTestSuite_test_stateKwd;
+
+static class TestDescription_ScannerTestSuite_test_gotoKwd : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_gotoKwd() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 54, "test_gotoKwd" ) {}
+ void runTest() { suite_ScannerTestSuite.test_gotoKwd(); }
+} testDescription_ScannerTestSuite_test_gotoKwd;
+
+static class TestDescription_ScannerTestSuite_test_whenKwd : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_whenKwd() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 57, "test_whenKwd" ) {}
+ void runTest() { suite_ScannerTestSuite.test_whenKwd(); }
+} testDescription_ScannerTestSuite_test_whenKwd;
+
+static class TestDescription_ScannerTestSuite_test_performingKwd : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_performingKwd() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 60, "test_performingKwd" ) {}
+ void runTest() { suite_ScannerTestSuite.test_performingKwd(); }
+} testDescription_ScannerTestSuite_test_performingKwd;
+
+static class TestDescription_ScannerTestSuite_test_exitKwd : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_exitKwd() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 63, "test_exitKwd" ) {}
+ void runTest() { suite_ScannerTestSuite.test_exitKwd(); }
+} testDescription_ScannerTestSuite_test_exitKwd;
+
+static class TestDescription_ScannerTestSuite_test_intKwd : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_intKwd() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 66, "test_intKwd" ) {}
+ void runTest() { suite_ScannerTestSuite.test_intKwd(); }
+} testDescription_ScannerTestSuite_test_intKwd;
+
+static class TestDescription_ScannerTestSuite_test_floatKwd : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_floatKwd() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 69, "test_floatKwd" ) {}
+ void runTest() { suite_ScannerTestSuite.test_floatKwd(); }
+} testDescription_ScannerTestSuite_test_floatKwd;
+
+static class TestDescription_ScannerTestSuite_test_booleanKwd : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_booleanKwd() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 72, "test_booleanKwd" ) {}
+ void runTest() { suite_ScannerTestSuite.test_booleanKwd(); }
+} testDescription_ScannerTestSuite_test_booleanKwd;
+
+static class TestDescription_ScannerTestSuite_test_stringKwd : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_stringKwd() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 75, "test_stringKwd" ) {}
+ void runTest() { suite_ScannerTestSuite.test_stringKwd(); }
+} testDescription_ScannerTestSuite_test_stringKwd;
+
+static class TestDescription_ScannerTestSuite_test_charKwd : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_charKwd() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 78, "test_charKwd" ) {}
+ void runTest() { suite_ScannerTestSuite.test_charKwd(); }
+} testDescription_ScannerTestSuite_test_charKwd;
+
+static class TestDescription_ScannerTestSuite_test_trueKwd : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_trueKwd() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 81, "test_trueKwd" ) {}
+ void runTest() { suite_ScannerTestSuite.test_trueKwd(); }
+} testDescription_ScannerTestSuite_test_trueKwd;
+
+static class TestDescription_ScannerTestSuite_test_falseKwd : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_falseKwd() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 84, "test_falseKwd" ) {}
+ void runTest() { suite_ScannerTestSuite.test_falseKwd(); }
+} testDescription_ScannerTestSuite_test_falseKwd;
+
+static class TestDescription_ScannerTestSuite_test_intConst : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_intConst() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 87, "test_intConst" ) {}
+ void runTest() { suite_ScannerTestSuite.test_intConst(); }
+} testDescription_ScannerTestSuite_test_intConst;
+
+static class TestDescription_ScannerTestSuite_test_floatConst : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_floatConst() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 90, "test_floatConst" ) {}
+ void runTest() { suite_ScannerTestSuite.test_floatConst(); }
+} testDescription_ScannerTestSuite_test_floatConst;
+
+static class TestDescription_ScannerTestSuite_test_stringConst : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_stringConst() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 93, "test_stringConst" ) {}
+ void runTest() { suite_ScannerTestSuite.test_stringConst(); }
+} testDescription_ScannerTestSuite_test_stringConst;
+
+static class TestDescription_ScannerTestSuite_test_charConst : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_charConst() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 96, "test_charConst" ) {}
+ void runTest() { suite_ScannerTestSuite.test_charConst(); }
+} testDescription_ScannerTestSuite_test_charConst;
+
+static class TestDescription_ScannerTestSuite_test_variableName : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_variableName() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 99, "test_variableName" ) {}
+ void runTest() { suite_ScannerTestSuite.test_variableName(); }
+} testDescription_ScannerTestSuite_test_variableName;
+
+static class TestDescription_ScannerTestSuite_test_leftParen : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_leftParen() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 102, "test_leftParen" ) {}
+ void runTest() { suite_ScannerTestSuite.test_leftParen(); }
+} testDescription_ScannerTestSuite_test_leftParen;
+
+static class TestDescription_ScannerTestSuite_test_rightParen : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_rightParen() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 105, "test_rightParen" ) {}
+ void runTest() { suite_ScannerTestSuite.test_rightParen(); }
+} testDescription_ScannerTestSuite_test_rightParen;
+
+static class TestDescription_ScannerTestSuite_test_leftCurly : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_leftCurly() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 108, "test_leftCurly" ) {}
+ void runTest() { suite_ScannerTestSuite.test_leftCurly(); }
+} testDescription_ScannerTestSuite_test_leftCurly;
+
+static class TestDescription_ScannerTestSuite_test_rightCurly : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_rightCurly() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 111, "test_rightCurly" ) {}
+ void runTest() { suite_ScannerTestSuite.test_rightCurly(); }
+} testDescription_ScannerTestSuite_test_rightCurly;
+
+static class TestDescription_ScannerTestSuite_test_leftAngle : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_leftAngle() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 114, "test_leftAngle" ) {}
+ void runTest() { suite_ScannerTestSuite.test_leftAngle(); }
+} testDescription_ScannerTestSuite_test_leftAngle;
+
+static class TestDescription_ScannerTestSuite_test_rightAngle : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_rightAngle() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 117, "test_rightAngle" ) {}
+ void runTest() { suite_ScannerTestSuite.test_rightAngle(); }
+} testDescription_ScannerTestSuite_test_rightAngle;
+
+static class TestDescription_ScannerTestSuite_test_colon : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_colon() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 120, "test_colon" ) {}
+ void runTest() { suite_ScannerTestSuite.test_colon(); }
+} testDescription_ScannerTestSuite_test_colon;
+
+static class TestDescription_ScannerTestSuite_test_comma : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_comma() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 123, "test_comma" ) {}
+ void runTest() { suite_ScannerTestSuite.test_comma(); }
+} testDescription_ScannerTestSuite_test_comma;
+
+static class TestDescription_ScannerTestSuite_test_semiColon : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_semiColon() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 126, "test_semiColon" ) {}
+ void runTest() { suite_ScannerTestSuite.test_semiColon(); }
+} testDescription_ScannerTestSuite_test_semiColon;
+
+static class TestDescription_ScannerTestSuite_test_assign : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_assign() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 129, "test_assign" ) {}
+ void runTest() { suite_ScannerTestSuite.test_assign(); }
+} testDescription_ScannerTestSuite_test_assign;
+
+static class TestDescription_ScannerTestSuite_test_plusSign : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_plusSign() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 132, "test_plusSign" ) {}
+ void runTest() { suite_ScannerTestSuite.test_plusSign(); }
+} testDescription_ScannerTestSuite_test_plusSign;
+
+static class TestDescription_ScannerTestSuite_test_star : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_star() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 135, "test_star" ) {}
+ void runTest() { suite_ScannerTestSuite.test_star(); }
+} testDescription_ScannerTestSuite_test_star;
+
+static class TestDescription_ScannerTestSuite_test_dash : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_dash() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 138, "test_dash" ) {}
+ void runTest() { suite_ScannerTestSuite.test_dash(); }
+} testDescription_ScannerTestSuite_test_dash;
+
+static class TestDescription_ScannerTestSuite_test_forwardSlash : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_forwardSlash() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 141, "test_forwardSlash" ) {}
+ void runTest() { suite_ScannerTestSuite.test_forwardSlash(); }
+} testDescription_ScannerTestSuite_test_forwardSlash;
+
+static class TestDescription_ScannerTestSuite_test_equalsEquals : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_equalsEquals() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 144, "test_equalsEquals" ) {}
+ void runTest() { suite_ScannerTestSuite.test_equalsEquals(); }
+} testDescription_ScannerTestSuite_test_equalsEquals;
+
+static class TestDescription_ScannerTestSuite_test_lessThanEquals : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_lessThanEquals() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 147, "test_lessThanEquals" ) {}
+ void runTest() { suite_ScannerTestSuite.test_lessThanEquals(); }
+} testDescription_ScannerTestSuite_test_lessThanEquals;
+
+static class TestDescription_ScannerTestSuite_test_greaterThanEquals : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_greaterThanEquals() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 150, "test_greaterThanEquals" ) {}
+ void runTest() { suite_ScannerTestSuite.test_greaterThanEquals(); }
+} testDescription_ScannerTestSuite_test_greaterThanEquals;
+
+static class TestDescription_ScannerTestSuite_test_notEquals : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_notEquals() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 153, "test_notEquals" ) {}
+ void runTest() { suite_ScannerTestSuite.test_notEquals(); }
+} testDescription_ScannerTestSuite_test_notEquals;
+
+static class TestDescription_ScannerTestSuite_test_endOfFile : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_endOfFile() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 156, "test_endOfFile" ) {}
+ void runTest() { suite_ScannerTestSuite.test_endOfFile(); }
+} testDescription_ScannerTestSuite_test_endOfFile;
+
+static class TestDescription_ScannerTestSuite_test_lexicalError : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_lexicalError() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 159, "test_lexicalError" ) {}
+ void runTest() { suite_ScannerTestSuite.test_lexicalError(); }
+} testDescription_ScannerTestSuite_test_lexicalError;
+
+static class TestDescription_ScannerTestSuite_test_scan_empty : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_scan_empty() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 265, "test_scan_empty" ) {}
+ void runTest() { suite_ScannerTestSuite.test_scan_empty(); }
+} testDescription_ScannerTestSuite_test_scan_empty;
+
+static class TestDescription_ScannerTestSuite_test_scan_empty_comment : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_scan_empty_comment() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 272, "test_scan_empty_comment" ) {}
+ void runTest() { suite_ScannerTestSuite.test_scan_empty_comment(); }
+} testDescription_ScannerTestSuite_test_scan_empty_comment;
+
+static class TestDescription_ScannerTestSuite_test_scan_lexicalErrors : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_scan_lexicalErrors() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 281, "test_scan_lexicalErrors" ) {}
+ void runTest() { suite_ScannerTestSuite.test_scan_lexicalErrors(); }
+} testDescription_ScannerTestSuite_test_scan_lexicalErrors;
+
+static class TestDescription_ScannerTestSuite_test_scan_nums_vars : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_scan_nums_vars() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 302, "test_scan_nums_vars" ) {}
+ void runTest() { suite_ScannerTestSuite.test_scan_nums_vars(); }
+} testDescription_ScannerTestSuite_test_scan_nums_vars;
+
+static class TestDescription_ScannerTestSuite_test_scan_bad_syntax_good_tokens : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_scan_bad_syntax_good_tokens() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 314, "test_scan_bad_syntax_good_tokens" ) {}
+ void runTest() { suite_ScannerTestSuite.test_scan_bad_syntax_good_tokens(); }
+} testDescription_ScannerTestSuite_test_scan_bad_syntax_good_tokens;
+
+static class TestDescription_ScannerTestSuite_test_scan_sample_abstar : public CxxTest::RealTestDescription {
+public:
+ TestDescription_ScannerTestSuite_test_scan_sample_abstar() : CxxTest::RealTestDescription( Tests_ScannerTestSuite, suiteDescription_ScannerTestSuite, 355, "test_scan_sample_abstar" ) {}
+ void runTest() { suite_ScannerTestSuite.test_scan_sample_abstar(); }
+} testDescription_ScannerTestSuite_test_scan_sample_abstar;
+
+#include <cxxtest/Root.cpp>
